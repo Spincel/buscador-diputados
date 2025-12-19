84b736230a8671471241569191d36f516f538a63
Spincel <hector.pincel@gmail.com>
2025-12-18 17:20:57 -0700

    avatar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,6 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
                     </x-nav-link>
                     @auth
                         @if(Auth::user()->role === 'admin')
@@ -84,7 +83,6 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
 
